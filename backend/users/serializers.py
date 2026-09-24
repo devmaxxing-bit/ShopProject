921e9e4b78e24f26f57a8a473ab625ec1fcd6100
devmaxxing-bit <maximartv06@gmail.com>
2026-09-24 17:21:14 +0500

    serializer edited v2

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -7,6 +7,8 @@ User = get_user_model()
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
+    #idk
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'first_name', 'last_name']
